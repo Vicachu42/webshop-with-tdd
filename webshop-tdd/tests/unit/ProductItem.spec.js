c26a9cd1eab06834e0a911a58b2162a6d53fa8a0
Vic Graf <victoria.b.graf@gmail.com>
2021-09-15 17:55:07 +0200

    Fixed bugging test

--- webshop-tdd/tests/unit/ProductItem.spec.js
+++ webshop-tdd/tests/unit/ProductItem.spec.js
@@ -134,149 +134,126 @@ describe('Life cycle hook', () => {
     const expected = 'Hakuho';
     const wrapper = shallowMount(ProductItem, {
       data: () => ({
-        products: {
-          products: [
-            {
-              id: 12,
-              shikona: 'Hakuho',
-              rank: 'Yokozuna',
-              height: '192 cm',
-              weight: '151 kg',
-              price: 'Â¥1 000 000',
-              picture: '../images/hakuho.jpg',
-              vicsMessage: 'I am the GOAT. Google me.',
-            },
-            {
-              id: 11,
-              shikona: 'Terunofuji',
-              rank: 'Yokozuna',
-              height: '192 cm',
-              weight: '184 kg',
-              price: 'Â¥1 000 000',
-              picture: '../images/terunofuji.jpg',
-            },
-            {
-              id: 10,
-              shikona: 'Takakeisho',
-              rank: 'Ozeki',
-              height: '175 cm',
-              weight: '183 kg',
-              price: 'Â¥900 000',
-              picture: '../images/takakeisho.jpg',
-            },
-            {
-              id: 9,
-              shikona: 'Mitakeumi',
-              rank: 'Sekiwake',
-              height: '179 cm',
-              weight: '172 kg',
-              price: 'Â¥800 000',
-              picture: '../images/mitakeumi.jpg',
-            },
-            {
-              id: 8,
-              shikona: 'Takayasu',
-              rank: 'Komusubi',
-              height: '187 cm',
-              weight: '177 kg',
-              price: 'Â¥700 000',
-              picture: '../images/takayasu.jpg',
-            },
-            {
-              id: 7,
-              shikona: 'Ichinojo',
-              rank: 'Komusubi',
-              height: '190 cm',
-              weight: '206 kg',
-              price: 'Â¥700 000',
-              picture: '../images/ichinojo.jpg',
-            },
-            {
-              id: 6,
-              shikona: 'Hoshoryu',
-              rank: 'Maegashira',
-              height: '187 cm',
-              weight: '132 kg',
-              price: 'Â¥500 000',
-              picture: '../images/hoshoryu.jpg',
-            },
-            {
-              id: 5,
-              shikona: 'Takanosho',
-              rank: 'Maegashira',
-              height: '184 cm',
-              weight: '163 kg',
-              price: 'Â¥500 000',
-              picture: '../images/takanosho.jpg',
-            },
-            {
-              id: 4,
-              shikona: 'Wakatakakage',
-              rank: 'Maegashira',
-              height: '180 cm',
-              weight: '129 kg',
-              price: 'Â¥500 000',
-              picture: '../images/wakatakakage.jpg',
-            },
-            {
-              id: 3,
-              shikona: 'Ura',
-              rank: 'Maegashira',
-              height: '176 cm',
-              weight: '147 kg',
-              price: 'Â¥500 000',
-              picture: '../images/ura.jpg',
-            },
-            {
-              id: 2,
-              shikona: 'Tobizaru',
-              rank: 'Maegashira',
-              height: '175 cm',
-              weight: '131 kg',
-              price: 'Â¥500 000',
-              picture: '../images/tobizaru.jpg',
-            },
-            {
-              id: 1,
-              shikona: 'Tochinoshin',
-              rank: 'Maegashira',
-              height: '192 cm',
-              weight: '175 kg',
-              price: 'Â¥500 000',
-              picture: '../images/tochinoshin.jpg',
-            },
-          ],
-        },
+        products: [
+          {
+            id: 12,
+            shikona: 'Hakuho',
+            rank: 'Yokozuna',
+            height: '192 cm',
+            weight: '151 kg',
+            price: 'Â¥1 000 000',
+            picture: '../images/hakuho.jpg',
+            vicsMessage: 'I am the GOAT. Google me.',
+          },
+          {
+            id: 11,
+            shikona: 'Terunofuji',
+            rank: 'Yokozuna',
+            height: '192 cm',
+            weight: '184 kg',
+            price: 'Â¥1 000 000',
+            picture: '../images/terunofuji.jpg',
+          },
+          {
+            id: 10,
+            shikona: 'Takakeisho',
+            rank: 'Ozeki',
+            height: '175 cm',
+            weight: '183 kg',
+            price: 'Â¥900 000',
+            picture: '../images/takakeisho.jpg',
+          },
+          {
+            id: 9,
+            shikona: 'Mitakeumi',
+            rank: 'Sekiwake',
+            height: '179 cm',
+            weight: '172 kg',
+            price: 'Â¥800 000',
+            picture: '../images/mitakeumi.jpg',
+          },
+          {
+            id: 8,
+            shikona: 'Takayasu',
+            rank: 'Komusubi',
+            height: '187 cm',
+            weight: '177 kg',
+            price: 'Â¥700 000',
+            picture: '../images/takayasu.jpg',
+          },
+          {
+            id: 7,
+            shikona: 'Ichinojo',
+            rank: 'Komusubi',
+            height: '190 cm',
+            weight: '206 kg',
+            price: 'Â¥700 000',
+            picture: '../images/ichinojo.jpg',
+          },
+          {
+            id: 6,
+            shikona: 'Hoshoryu',
+            rank: 'Maegashira',
+            height: '187 cm',
+            weight: '132 kg',
+            price: 'Â¥500 000',
+            picture: '../images/hoshoryu.jpg',
+          },
+          {
+            id: 5,
+            shikona: 'Takanosho',
+            rank: 'Maegashira',
+            height: '184 cm',
+            weight: '163 kg',
+            price: 'Â¥500 000',
+            picture: '../images/takanosho.jpg',
+          },
+          {
+            id: 4,
+            shikona: 'Wakatakakage',
+            rank: 'Maegashira',
+            height: '180 cm',
+            weight: '129 kg',
+            price: 'Â¥500 000',
+            picture: '../images/wakatakakage.jpg',
+          },
+          {
+            id: 3,
+            shikona: 'Ura',
+            rank: 'Maegashira',
+            height: '176 cm',
+            weight: '147 kg',
+            price: 'Â¥500 000',
+            picture: '../images/ura.jpg',
+          },
+          {
+            id: 2,
+            shikona: 'Tobizaru',
+            rank: 'Maegashira',
+            height: '175 cm',
+            weight: '131 kg',
+            price: 'Â¥500 000',
+            picture: '../images/tobizaru.jpg',
+          },
+          {
+            id: 1,
+            shikona: 'Tochinoshin',
+            rank: 'Maegashira',
+            height: '192 cm',
+            weight: '175 kg',
+            price: 'Â¥500 000',
+            picture: '../images/tochinoshin.jpg',
+          },
+        ],
       }),
     });
 
     const nameElem = wrapper.find('h4');
     const text = nameElem.text();
+    console.log(text);
 
     expect(text).toBe(expected);
   });
-
-  // it('should show the results from the fetch call correctly', async () => {
-  //   const expected = 'Terunofuji';
-  //   const wrapper = shallowMount(ProductItem);
-
-  //   await wrapper.setData({
-  //     products: {
-  //       id: 9,
-  //       shikona: 'Terunofuji',
-  //       rank: 'Yokozuna',
-  //       height: '192 cm',
-  //       weight: '184 kg',
-  //       price: 'Â¥1 000 000',
-  //       picture: '../images/terunofuji.jpg',
-  //     },
-  //   });
-
-  //   const shikonaElem = wrapper.find('h4');
-  //   const text = shikonaElem.text();
-  //   console.log(text);
-  //   expect(text).toBe(expected);
-  // });
 
   it('should add a product to the cart', () => {});
 
